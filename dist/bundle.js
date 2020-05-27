94f095a6bc0c4165eb7ecff52f78c95e5a869d27
Fabio Ricali <ricalifabio@gmail.com>
2020-05-27 16:26:14 +0200

    BUILD

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,4 +1,4 @@
-// [DozTab]  Build version: 1.0.2  
+// [DozTab]  Build version: 1.1.0  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory(require("doz"));
